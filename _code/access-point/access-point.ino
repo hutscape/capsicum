93974152c845cedac5ab0f9ac607410adc0cd921
Sayanee <sayanee@gmail.com>
2024-04-12 19:55:27 +0800

    feat (firmware): add tutorial on creating a simple access point to view a webpage

---
+++ _code/access-point/access-point.ino
@@ -0,0 +1,59 @@
+// https://raw.githubusercontent.com/espressif/arduino-esp32/990e3d5b431b63b4adc364b045a79afdad645a3f/libraries/WiFi/examples/WiFiAccessPoint/WiFiAccessPoint.ino
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
+#define LED 3
+
+const char *ssid = "batt";
+const char *password = "12345678";
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Configuring access point...");
+
+  pinMode(LED, OUTPUT);
+  pinMode(BATTERY_ENABLE_PIN, HIGH);
+
+  if (!WiFi.softAP(ssid, password)) {
+    log_e("Soft AP creation failed.");
+    while (1) {}
+  }
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+
+  Serial.println("Server started");
+  Serial.print("Battery Analog level: ");
+  Serial.println(measureBatteryVoltage());
+}
+
+void loop() {
+  WiFiClient client = server.accept();
+
+  if (client) {
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        digitalWrite(LED, HIGH);
+
+        client.println("HTTP/1.1 200 OK");
+        client.println("Content-type:text/html");
+        client.println();
+
+        client.print("Hello World!");
+
+        client.println();
+        break;
+      }
+    }
+    client.stop();
+    Serial.println("Client Disconnected.");
+    digitalWrite(LED, LOW);
+  }
+}
